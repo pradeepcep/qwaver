0ac2eb3a1ef7743522c2fdbc4d13957ddfa7b606
Brian Risk <brian@geneffects.com>
2022-05-25 20:00:16 -0400

    adjusting stylings

--- queries/models.py
+++ queries/models.py
@@ -4,6 +4,18 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class Database(models.Model):
+    title = models.CharField(max_length=256, default="")
+    host = models.CharField(max_length=256)
+    port = models.IntegerField(default=1433)
+    database = models.CharField(max_length=256, default="")
+    user = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
+
+
 class Query(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
