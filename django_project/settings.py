be1849c02f37275158861101570d838b00fb9d0f
Brian Risk <brian@geneffects.com>
2022-07-13 07:42:02 -0400

    getting WowMyData.com online

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ notes.md
@@ -0,0 +1,6 @@
+# Working with cPanel
+/home/geneffec/virtualenv/wowmydata/3.7/bin/pip install --upgrade pip
+
+/home/geneffec/virtualenv/wowmydata/3.7/bin/pip3 install -r requirements.txt
+
+source /home/geneffec/virtualenv/wowmydata/3.8/bin/activate && cd /home/geneffec/wowmydata
