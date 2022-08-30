98f1f6dd530f965052f5d80a632dbdadbb8f670a
Brian Risk <brian@geneffects.com>
2022-08-29 22:24:10 -0400

    removing misplaced comma

--- qwaver/settings.py
+++ qwaver/settings.py
@@ -46,7 +46,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.get('config', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config.get('config', 'DEBUG') == 'True',
+DEBUG = config.get('config', 'DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
