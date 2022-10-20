ba49d9ccf6a4d5fa7c201a029735f90811cfcb2f
Brian Risk <brian@geneffects.com>
2022-10-20 06:30:22 -0400

    updating MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')

--- qwaver/settings.py
+++ qwaver/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
