4c5412339377bae0a834a6609aa86388d2a57023
Brian Risk <brian@geneffects.com>
2022-11-11 07:43:58 -0500

    version history with diff

--- queries/views/query_version.py
+++ queries/views/query_version.py
@@ -1,3 +1,5 @@
+import difflib
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
@@ -15,8 +17,26 @@ class QueryVersionListView(LoginRequiredMixin, ListView):
         user = self.request.user
         query = get_object_or_404(Query, id=self.kwargs.get('query_id'))
         user_can_access_query(user, query)
-        versions = QueryVersion.objects.filter(query=query).order_by('-timestamp')
-        return versions
+        versions = QueryVersion.objects.filter(query=query).order_by('timestamp')
+        queryset = []
+        for index, version in enumerate(versions):
+            line = {'version': version}
+            if index == 0:
+                line['diff'] = "original"
+            else:
+                before = versions[index - 1].query_text
+                after = versions[index].query_text
+                diff = ""
+                for i, s in enumerate(difflib.ndiff(before, after)):
+                    if s[0] == ' ':
+                        continue
+                    elif s[0] == '-':
+                        diff = diff + f"<span style='color: red'>{s[-1]}</span>"
+                    elif s[0] == '+':
+                        diff = diff + f"<span style='color: green'>{s[-1]}</span>"
+                line['diff'] = diff
+            queryset.append(line)
+        return queryset
 
     # printing difference: https://stackoverflow.com/questions/17904097/python-difference-between-two-strings
     def get_context_data(self, **kwargs):
