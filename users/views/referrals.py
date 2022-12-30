16b6d483c2b88d892b72fe2d657252fad5ce412e
Brian Risk <brian@geneffects.com>
2022-12-30 07:00:30 -0500

    more info in referrals list

--- users/views/referrals.py
+++ users/views/referrals.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from django.views.generic import (
     ListView,
