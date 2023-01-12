335304e3a224e097f9e0a8cd76575a7c1a94d8fa
Brian Risk <brian@geneffects.com>
2023-01-12 17:59:51 -0500

    updating logout page

--- users/urls.py
+++ users/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/<str:ref_code>', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='queries/about.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='queries/index.html'), name='logout'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset.html'
